411af9c74d18672888d048721bdbd222564875bc
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-07-10 11:34:13 -0300

    neww commit

---
+++ vue-start-spa/src/components/PageViewer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h1 class="emphasized">{{page.pageTitle}}</h1>
+        <p>{{page.content}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['page']
+    }
+</script>
+
+<style scoped>
+.emphasized {
+    color: blue;
+}
+</style>
