e3fec65992e337ad3e3f5473d08d9adc06e27414
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-07-10 11:59:11 -0300

    new changes

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -25,23 +25,14 @@ export default{
     return {
       activePage: 0,
       pages: [
-        {
-          link: {text: 'Home', url: 'index.html'},
-          pageTitle: 'Home Page',
-          content: 'This is the home content'
-        },
-        {
-          link: {text: 'About', url: 'about.html'},
-          pageTitle: 'About Page',
-          content: 'This is the about content'
-        },
-        {
-          link: {text: 'Contact', url: 'contact.html'},
-          pageTitle: 'Contact Page',
-          content: 'This is the contact content'
-        }
+        
       ]
     }
   },
+  methods: {
+    async getPages() {
+      let res = await fetch('pages.json');
+    }
+  }
 }
 </script>
