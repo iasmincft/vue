2e5ddf5121032646e132a425f37ba2ece5746b7a
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-07-11 09:15:18 -0300

    new

--- vue-start-spa/src/main.js
+++ vue-start-spa/src/main.js
@@ -1,5 +1,8 @@
 import {createApp, VueElement} from 'vue';
 import App from './App.vue';
 import'../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 
 createApp(App).mount('#app');
