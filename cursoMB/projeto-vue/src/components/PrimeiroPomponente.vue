a0e17fe0199875eb4670a2ec739d5ae8e6f6d321
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-07-15 11:38:37 -0300

    new

---
+++ cursoMB/projeto-vue/src/components/PrimeiroPomponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>titulo</h1>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroPomponente',
+        data() {
+            return {
+                mensagem: 'Olá, Vue!'
+            }
+        },
+        methods: {
+            exibirMensagem() {
+                alert(this.mensagem);
+            }
+        }
+    }
+</script>
