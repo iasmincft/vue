908fa3e97847aef54f19c0f570b126f5dbe623cc
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-07-15 09:50:11 -0300

    new

--- vue-start-spa/src/main.js
+++ vue-start-spa/src/main.js
@@ -2,8 +2,10 @@ import {createApp, VueElement} from 'vue';
 import App from './App.vue';
 import'../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import $bus from './utils/Events';
+import router from './routes';
 
 
 const app = createApp(App);
+app.use(router);
 app.config.globalProperties.$bus = $bus;
 app.mount('#app');
