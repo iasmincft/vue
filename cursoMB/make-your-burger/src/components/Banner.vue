aa2a75ffce103b0da91ba79329c29ff24ece0bd0
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-07-21 12:11:24 -0300

    new

---
+++ cursoMB/make-your-burger/src/components/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main-banner">
+    <h1>Make Your Burger</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+
+</script>
+
+<style scoped>
+
+</style>
